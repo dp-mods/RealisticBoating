6ab0a42c13e3e5d283471c3fba46b1c461208fe7
dp-mods <72931225+dp-mods@users.noreply.github.com>
2020-11-08 02:00:21 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
 Config = {}
 
-
+Config.ExpandOceanLimit = true -- If you want to increase the distance of the invisble boundary set by the game
 Config.UsePNotify = true -- Set this to false if you're not using pNofity!
