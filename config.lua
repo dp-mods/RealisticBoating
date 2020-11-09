5592967f01661bd0f5098692718bc9cc9c9efd26
dp-mods <72931225+dp-mods@users.noreply.github.com>
2020-11-09 02:29:59 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,8 @@
 Config = {}
 
 Config.KeyBind = 74 -- Default is H
-Config.WaveIntensity = 2.25 -- Must be a float value between 1.0 & 3.0 - Set to 0 for default GTA waves
+Config.WaterStrength = 1.2 -- Must be a value between 1.0 & 3.0 - However, you can set it to 0 for default water strength
 Config.ImprovedWaterConditions = true
 Config.UsePNotify = true -- Set this to false if you're not using pNofity!
 Config.SinkWhenSubmerged = true -- If boats sink when they become fully submerged in water or not
-Config.SubmergmentRate = 0.92 -- Change this to whatever you like, but most boats tend to be submerged at around 0.6/0.7 when idle
+Config.SubmergmentRate = 0.97 -- Change this to whatever you like, but most boats tend to be submerged at around 0.6/0.7 when idle
